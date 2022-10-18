c7e3e9bee591dbfb420f1994b47b2faabf2f0645
Rick Vermeil <rickv85@gmail.com>
2022-10-18 15:25:31 -0600

    Complete all tests

--- tweet.js
+++ tweet.js
@@ -1,5 +1,24 @@
 class Tweet {
-
+    constructor(sender, content, link ='no link provided',
+     likes = 0, retweets = 0, replies = [], replyCount = 0) {
+        this.sender = sender;
+        this.content = content;
+        this.link = link;
+        this.likes = likes;
+        this.retweets = retweets;
+        this.replies = replies;
+        this.replyCount = replyCount;
+    }
+    like() {
+        return this.likes += 1;
+    }
+    retweet() {
+        return this.retweets += 1;
+    }
+    reply(reply) {
+        this.replyCount += 1;
+        return this.replies.push(reply);
+    }
 }
 
 module.exports = Tweet;
